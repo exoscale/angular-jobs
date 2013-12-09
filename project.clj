e7d729006be7a4ee9e853c4a204f17c8dd025441
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-12-10 00:58:15 +0100

    add marginalia plugin

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main io.exo.jobs
+  :plugins [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure       "1.5.1"]
                  [http-kit                  "2.1.13"]
                  [compojure                 "1.1.6"]
